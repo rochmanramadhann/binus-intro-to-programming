5f7bb449cfaa62ae291cf8bdbe6c3dc18e0781b9
rochmanramadhani12 <rochmanramadhani.dev@gmail.com>
2022-10-04 21:57:26 +0700

    finish CustomException.java on package forum.session_10

---
+++ src/forum/session_10/CustomException.java
@@ -0,0 +1,49 @@
+package forum.session_10;
+
+public class CustomException {
+    // class representing custom exception
+    static class InvalidAgeException  extends Exception
+    {
+        public InvalidAgeException (String str)
+        {
+            // calling the constructor of parent Exception
+            super(str);
+        }
+    }
+
+    // class that uses custom exception InvalidAgeException
+    public static class TestCustomException
+    {
+
+        // method to check the age
+        static void validate(int age) throws InvalidAgeException{
+            if(age < 18){
+
+                // throw an object of user defined exception
+                throw new InvalidAgeException("age is not valid to vote");
+            }
+            else {
+                System.out.println("welcome to vote");
+            }
+        }
+
+        // main method
+        public static void main(String args[])
+        {
+            try
+            {
+                // calling the method
+                validate(13);
+            }
+            catch (InvalidAgeException ex)
+            {
+                System.out.println("Caught the exception");
+
+                // printing the message from InvalidAgeException object
+                System.out.println("Exception occured: " + ex);
+            }
+
+            System.out.println("rest of the code...");
+        }
+    }
+}
